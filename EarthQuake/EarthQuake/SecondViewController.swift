25245f652ca28ffce067e0ce9fb7d99b557453e7
customsoftware <kcluff50@gmail.com>
2019-02-12 14:53:54 -0700

    Initial

---
+++ EarthQuake/EarthQuake/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  EarthQuake
+//
+//  Created by Kenneth Cluff on 2/12/19.
+//  Copyright © 2019 Kenneth Cluff. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
