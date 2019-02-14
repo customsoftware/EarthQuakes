ca99574cd6ec31719c1a3891dad9a661cd626f43
Ken Cluff <kcluff50@gmail.com>
2019-02-14 08:07:52 -0700

    Working zoom

--- EarthQuake/EarthQuake/Controllers/HomeSplitViewController.swift
+++ EarthQuake/EarthQuake/Controllers/HomeSplitViewController.swift
@@ -29,7 +29,7 @@ class HomeSplitViewController: UISplitViewController {
 extension HomeSplitViewController: UISplitViewControllerDelegate {
     func splitViewController(_ splitViewController: UISplitViewController, collapseSecondary secondaryViewController:UIViewController, onto primaryViewController:UIViewController) -> Bool {
         guard let secondaryAsNavController = secondaryViewController as? UINavigationController,
-            let _ = secondaryAsNavController.topViewController as? SecondViewController else { return true }
-        return false
+            let _ = secondaryAsNavController.topViewController as? SecondViewController else { return false }
+        return true
     }
 }
